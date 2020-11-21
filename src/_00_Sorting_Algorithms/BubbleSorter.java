a7f4e5b5cda67916bde0aec5564178105eb87fb7
horndjellyfish <hornedjellyfish@gmail.com>
2020-11-21 11:30:55 -0800

    Swapping Digits - complete, Sorting Algorithms - incomplete
    day 2

--- src/_00_Sorting_Algorithms/BubbleSorter.java
+++ src/_00_Sorting_Algorithms/BubbleSorter.java
@@ -10,7 +10,16 @@ public class BubbleSorter extends Sorter{
 	//   progress on the graph.
 	@Override
 	void sort(int[] array, SortingVisualizer display) {
-		
+		for (int i = 0; i < array.length-1; i++) {
+			for (int j = 0; j < array.length-1; j++) {
+				if (array[j] > array[j+1]) {
+					int k = array[j];
+					array[j] = array[j+1];
+					array[j+1] = k;
+				}
+			}
+		}
+		display.updateDisplay();
 	}
 	
 }
