7c5b628886a36c4f8b2b1a5b68fcebf23bc774af
horndjellyfish <hornedjellyfish@gmail.com>
2021-01-09 11:20:17 -0800

    RecursionMath - complete, StringTrimmer - complete
    day 9

--- src/_99_extra/recursion_practice/RecursionMath.java
+++ src/_99_extra/recursion_practice/RecursionMath.java
@@ -19,21 +19,13 @@ public class RecursionMath {
 
     //6. Try this one on your own!
     //Hint: if numberToDivideBy is bigger than number, you can't divide anymore
-    public static int recursiveDivision(int number, int numberToDivideBy) {
-        if (numberToDivideBy == 1) {
-            return number;
-        } else {
-            return number - recursiveDivision(number, numberToDivideBy - 1);
-        }
-    }
 
     public static int Division(int n, int b) {
         int result;
-        if (n - b < n) {
-            result = n;
-        }
-        else {
-            result = n - Division(b, (n / b));
+        if (n < b) {
+            result = 0;
+        } else {
+            result = 1 + Division(n - b, b);
         }
         return result;
     }
@@ -42,9 +34,9 @@ public class RecursionMath {
     //8. Try this one on your own!
     public static int recursivePower(int number, int power) {
         if (power == 0) {
-            return -1;
+            return 1;
         } else {
-            return number + recursivePower(number, power - 1);
+            return number * recursivePower(number, power - 1);
         }
     }
 
@@ -62,14 +54,17 @@ public class RecursionMath {
     public void testDivision() {
         //5  Add JUnit tests to test your method
         assertEquals(2, Division(2, 1));
-        assertEquals(4, recursiveDivision(8, 2));
-        assertEquals(6, recursiveDivision(66, 11));
+        assertEquals(4, Division(8, 2));
+        assertEquals(6, Division(66, 11));
     }
 
 
     @Test
     public void testPower() {
         //7  Add JUnit tests to test your method
+        assertEquals(25, recursivePower(5, 2));
+        assertEquals(9, recursivePower(3, 2));
+        assertEquals(32, recursivePower(2, 5));
 
     }
 
--- src/_99_extra/recursion_practice/StringTrimmer.java
+++ src/_99_extra/recursion_practice/StringTrimmer.java
@@ -3,20 +3,28 @@ package _99_extra.recursion_practice;
 import static org.junit.Assert.assertEquals;
 
 public class StringTrimmer {
-	
-	static public String trimString(String str, int removesLeft) {
-		//1. If there are no removes left
-			//2.  return the string
-		
-		//3. Else you need to substring the string by 1, reduce removesLeft by 1, and then return both. 
-		
-		return null;
-	}
-	
-	public static void main(String[] args) {
-		assertEquals("Hello W", trimString("Hello World!", 5));
-		assertEquals("League", trimString("LeagueOfAmazing!", 10));
-		assertEquals("James", trimString("JamesTheIntern", 9));
-		assertEquals("", trimString("TheWholeWord", 12));
-	}
+
+    static public String trimString(String str, int removesLeft) {
+        //1. If there are no removes left
+        if (removesLeft == 0) {
+            return str;
+        }
+        //2.  return the string
+
+        //3. Else you need to substring the string by 1, reduce removesLeft by 1, and then return both.
+        else {
+        	String message = str.substring(0, str.length() - 1);
+        	removesLeft--;
+			System.out.println(message);
+            return trimString(message, removesLeft);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        assertEquals("Hello W", trimString("Hello World!", 5));
+        assertEquals("League", trimString("LeagueOfAmazing!", 10));
+        assertEquals("James", trimString("JamesTheIntern", 9));
+        assertEquals("", trimString("TheWholeWord", 12));
+    }
 }
